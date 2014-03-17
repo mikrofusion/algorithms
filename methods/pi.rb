b863e11d2003dd1731cd8e545ce880de006f949f
Mike Groseclose <mike.groseclose@gmail.com>
2014-03-16 21:25:59 -0700

    add pi

---
+++ methods/pi.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/ruby
+
+def pi_recursive(depth, count, j)
+  if count >= depth then return 0 end
+
+  r = 4.0 / (j..(j + 2)).to_a.inject(1) { |sum, x| sum * x }
+  pi_recursive(depth, count + 1, j + 2) +
+    if count % 2 == 1 then + r else - r end
+end
+
+def pi(depth) # using Nilakantha series
+  pi_recursive(depth, 1, 2) + 3
+end
+
+puts "#{pi(1000)}"
