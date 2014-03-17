591cb3f5bac1dd3950770caa1833e4b197a80ab2
Mike Groseclose <mike.groseclose@gmail.com>
2014-03-16 21:26:45 -0700

    add pi

--- methods/pi.rb
+++ methods/pi.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
 
-def pi_recursive(depth, count, j)
+def pi_recursive(depth, count, i)
   if count >= depth then return 0 end
 
-  r = 4.0 / (j..(j + 2)).to_a.inject(1) { |sum, x| sum * x }
+  r = 4.0 / (i..(i + 2)).to_a.inject(1) { |sum, x| sum * x }
   pi_recursive(depth, count + 1, j + 2) +
     if count % 2 == 1 then + r else - r end
 end
