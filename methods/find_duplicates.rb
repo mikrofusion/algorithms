b0b05cac92468d61f8295b6e5621f1cd544fd517
Mike Groseclose <mike.groseclose@gmail.com>
2014-03-16 22:15:38 -0700

    add find duplicates

--- methods/find_duplicates.rb
+++ methods/find_duplicates.rb
@@ -1,16 +1,16 @@
 #!/usr/bin/ruby
 
 def find_duplicates(arr) # O(n)
-  hash = {}
+  hash = Hash.new(0)
   duplicates = []
 
   arr.each do |val|
-    if hash.has_key? val then duplicates.push val end
-    hash[val] = true
+    if hash.has_key? val and hash[val] == 1 then duplicates.push val end
+    hash[val] += 1
   end
 
-  duplicates
+  duplicates.map { |x| "#{x} appears #{hash[x]} times" }
 end
 
-arr = [1, 3, 2, 5, 6, 6, 7]
+arr = [1, 3, 2, 5, 6, 6, 7, 3, 3]
 puts "#{find_duplicates(arr)}"
